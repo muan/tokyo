dba6d6ca00403a966303ec97e060d9527d584748
Mu-An Chiou <me@muanchiou.com>
2014-10-19 16:07:25 +0900

    Check if dir exist

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ var fs = require('fs')
 var datetime = (new Date).toISOString()
 var html = '<h1>Tokyo</h1><time>' + datetime + '</time><p>Such city.</p>'
 
-fs.mkdir('./site', function(err) {
-  if (err) return console.log(err)
+if(fs.existsSync('./site')) {
+  writeFile()
+} else {
+  fs.mkdir('./site', function(err) {
+    writeFile()
+    if (err) return console.log(err)
+  })
+}
 
+function writeFile() {
   fs.writeFile('./site/index.html', html, function (err) {
     if (err) return console.log(err)
   })
-})
+}
